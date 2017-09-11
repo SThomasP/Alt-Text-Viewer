25e196d4993e30eb649edd5125850093578a4e40
Steffan P <stp1g15@soton.ac.uk>
2017-09-11 16:04:18 +0100

    Initial and final commit for the version 1 release.

---
+++ show-alt-text.js
@@ -0,0 +1,39 @@
+function getImagesWithAlt(){
+    var x = document.getElementsByTagName("img");
+    var i;
+    var y = [];
+    for (i =0; i<x.length; i++) {
+        if ( x[i].title != ""){
+          y.push(x[i]);
+        }
+    }
+    return y;
+}
+
+function createBoundingBox(){
+    node = document.createElement("div");
+    node.style = "outline-color:lightgrey;outline-style:dashed;"
+    return node;
+}
+
+function createParagraph(){
+    paragraph = document.createElement("p");
+    paragraph.style = "text-align:center;font-family:sans-serif;font-size:12px;color:black;background-color:lemonchiffon;"
+    return paragraph;
+}
+
+function seperateTitle(image){
+    node = createBoundingBox();
+    alt = createParagraph();
+    alt.appendChild(document.createTextNode(image.title));
+    parent = image.parentNode;
+    parent.replaceChild(node, image);
+    node.appendChild(image);
+    node.appendChild(alt);
+}
+
+var altImages = getImagesWithAlt();
+for (i=0; i < altImages.length; i ++){
+    seperateTitle(altImages[i]);
+}
+
