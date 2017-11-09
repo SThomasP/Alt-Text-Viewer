16e39101cf3a65604ac465fe6c72991822e60e82
Steffan P <stp1g15@soton.ac.uk>
2017-11-09 13:34:45 +0000

    Version 3.0 Working ability to set minimum value and restore it default.

--- backgroundScript.js
+++ backgroundScript.js
@@ -5,20 +5,24 @@ var defaults = {
   min: 39000
 };
 
-//Check if stored, if not set to default.
+
 
 function onError(e){
   console.error(e);
 }
 
+
+// to be run when the app is installed.
 function initStorage(settings){
-  var keys = Object.keys(defaults);
-  for (var i = 0; i < keys.length;i++){
-    var nthKey = keys[i];
-    var storage = {};
-    storage[nthKey] = defaults[nthKey];
-    browser.storage.local.set(storage);
-    console.log(nthKey+ ' set');
+  if (settings.reason === "install"){
+    var keys = Object.keys(defaults);
+    for (var i = 0; i < keys.length;i++){
+      var nthKey = keys[i];
+      var storage = {};
+      storage[nthKey] = defaults[nthKey];
+      browser.storage.local.set(storage);
+      console.log(nthKey+ ' set');
+    }
   }
   browser.storage.local.set({defaults: defaults});
   console.log("default values set.");
--- options_page/editOptions.js
+++ options_page/editOptions.js
@@ -1,8 +1,25 @@
 
 function save(event){
+  var toSet = document.querySelector('#min_size').value;
+  var setting = browser.storage.local.set({min: toSet});
   event.preventDefault();
 }
 
-function restoreDefault() {
+function restoreDefault(event){
+ var getting = browser.storage.local.get('defaults');
+ getting.then((res) => {
+   document.querySelector('#min_size').value = res.defaults.min;
+ })
 
 }
+
+function restore(){
+  var getting = browser.storage.local.get('min');
+  getting.then((res) => {
+    document.querySelector('#min_size').value = res.min;
+  });
+}
+
+document.addEventListener("DOMContentLoaded", restore);
+document.querySelector('form').addEventListener("submit", save);
+document.querySelector('#minDefault').addEventListener("click", restoreDefault);
